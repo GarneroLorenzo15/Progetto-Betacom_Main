4a6909672d9b63b20af453e36927675a82c9053a
GarneroLorenzo15 <garnerolorenzo1@gmail.com>
2024-02-05 16:12:35 +0100

    finiti server e db

--- progettoBetacom/Backend/serverProgetto.js
+++ progettoBetacom/Backend/serverProgetto.js
@@ -1,34 +1,43 @@
 const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
 const mysql = require('mysql');
 const port = 3000;
 
 const app = express();
+
+app.use(cors());
+app.use(morgan('tiny')); //stampa nel terminale le richieste che arrivano nel server
+app.use(express.json()); //per tradurre i json in arrivo nei pacchetti destinati al nostro server
+
+
 const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'Lollo9090!',
     port: '3306',
-    database: 'prgettobetacomdb'
+    database: 'databasebetacomgioco'
 });
-
-app.set('view engine', 'ejs');
 
 
 app.get('/', (req, res) =>{
     res.send('works');
 });;
 
-/* app.get('/utente', (req, res) =>{
-    for(let i = 0; i < 27; i++){
-        connection.query('SELECT * FROM prgettobetacomdb.utente (nome) VALUES (?)', [i],  (err, result) =>{
-            if(err) throw err;
-            res.render('../Frontend/views/utente', {utente: result});
-        })
-    }
-    res.sendFile('../Frontend/views/utente', {utente: result});
+app.get('/api/utenti', (req, res) => {
+    connection.query('SELECT * FROM utente', (err, rows) => {
+        if (err) throw err;
+        res.json(rows);
+    });
 });
 
- */
+app.get('/api/eventi', (req, res) => {
+    connection.query('SELECT * FROM evento', (err, rows) => {
+        if (err) throw err;
+        res.json(rows);
+    });
+});
+
+
 app.listen(port, () =>{
     console.log(`Server is running on port ${port}`)
 });;
