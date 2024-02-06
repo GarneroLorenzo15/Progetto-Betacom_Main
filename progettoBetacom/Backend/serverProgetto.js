be04af3346393f9d61fee2e8908e294a947730e6
GarneroLorenzo15 <garnerolorenzo1@gmail.com>
2024-02-06 11:57:56 +0100

    apis

--- progettoBetacom/Backend/serverProgetto.js
+++ progettoBetacom/Backend/serverProgetto.js
@@ -30,15 +30,37 @@ app.get('/api/utenti', (req, res) => {
     });
 });
 
-app.get('api/addUtente', (req, res) => {
-    connection.query('INSERT INTO utente (nome, cognome, email, password) VALUES (?,?,?,?)', [req.query.nome, req.query.cognome, req.query.email, req.query.password], (err, rows)=>{
+
+app.get('/api/utenti/delete/:id', (req, res) => {
+    const userid = req.params.id;
+    connection.query('DELETE FROM utente WHERE id_Utente = ?', [userid], (err, rows) => {
+        if (err) throw err;
+        res.json({message: `l'utente ${userid} è stato rimosso`});
+    });
+});
+
+app.post('/api/utenti/add', (req, res) => {
+    connection.query('INSERT INTO utente (id_Utente, nome, cognome, email, password) VALUES (?, ?, ?, ?, ?)',[1, 'Sandro', 'Maffiodo', 's.maffiodo', 'sandro'], (err, rows)=>{
         if(err) throw err;
-        res.json(rows);
+        res.json({message: 'Utente inserito correttamente!'});
     });
 });
 
 app.get('/api/eventi', (req, res) => {
     connection.query('SELECT * FROM evento', (err, rows) => {
+        if (err) throw err;
+        res.json(rows);
+    });
+});
+app.get('/api/eventi/delete/:id', (req, res) => {
+    const eventid = req.params.id;
+    connection.query('DELETE FROM evento WHERE id_Evento = ?', [eventid], (err, rows) => {
+        if (err) throw err;
+        res.json({message: `l'evento ${eventid} è stato rimosso`});
+    });
+});
+app.post('/api/eventi/add', (req, res) => {
+    connection.query('INSERT INTO evento (id_Evento, titolo, data, descrizione, luogo, immagine_evento) VALUES (?, ?, ?, ?, ?, ?)',[5, 'piscina', '2024-04-21', 'Luogo molto divertente ma anche rilassante.', '', ''], (err, rows) => {
         if (err) throw err;
         res.json(rows);
     });
