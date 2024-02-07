f8cef915206d3133f4f0e46d133dffdeca920676
GarneroLorenzo15 <garnerolorenzo1@gmail.com>
2024-02-07 16:11:33 +0100

    tailwind

--- progettoBetacom/Frontend/progetto-betacom/src/views/AboutView.vue
+++ progettoBetacom/Frontend/progetto-betacom/src/views/AboutView.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1 class="w-full text-center text-green-900">This is an about page</h1>
 
     <p>perchè non puoi andare in modo normale</p>
   </div>
