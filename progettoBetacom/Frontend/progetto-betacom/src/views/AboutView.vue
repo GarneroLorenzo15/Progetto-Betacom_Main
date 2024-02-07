d2fa049a77eca45a0a758216d83a1c75c9653491
GarneroLorenzo15 <garnerolorenzo1@gmail.com>
2024-02-07 16:26:16 +0100

    tailwind install

--- progettoBetacom/Frontend/progetto-betacom/src/views/AboutView.vue
+++ progettoBetacom/Frontend/progetto-betacom/src/views/AboutView.vue
@@ -1,7 +1,20 @@
 <template>
   <div class="about">
-    <h1 class="w-full text-center text-green-900">This is an about page</h1>
+    <h1 class="w-full lg:text-xl">This is an about page</h1>
 
     <p>perchè non puoi andare in modo normale</p>
   </div>
 </template>
+
+
+<script>
+
+</script>
+
+
+<style scoped>
+h1{
+  color: green;
+}
+
+</style>
