8103e9941f8df0d2c0b58b46ec8ca45fdf95bfd6
GarneroLorenzo15 <garnerolorenzo1@gmail.com>
2024-02-07 17:49:25 +0100

    inizializzazione ambiente di sviluppo front

---
+++ progettoBetacom/Frontend/progetto-betacom-front/src/components/SecondPage.vue
@@ -0,0 +1,21 @@
+<template class="bg-red-500">
+  <div class="second">
+    <h1 class="w-full ">{{ msg }}</h1>
+    <p class="w-full h-full bg-red-600">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde perferendis
+      ad ratione excepturi dolore autem minus dicta vel, rem dolorum temporibus,
+      sapiente tempore quibusdam porro aperiam mollitia sed natus. Eos.
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SecondPage",
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<style scoped></style>
