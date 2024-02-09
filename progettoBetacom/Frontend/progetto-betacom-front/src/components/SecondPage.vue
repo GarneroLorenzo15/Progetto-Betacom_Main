0b00a199af8a1cbbb80dd44369440c5c011ec55f
GarneroLorenzo15 <garnerolorenzo1@gmail.com>
2024-02-09 14:52:01 +0100

    prove familiarizzazione con vue

--- progettoBetacom/Frontend/progetto-betacom-front/src/components/SecondPage.vue
+++ progettoBetacom/Frontend/progetto-betacom-front/src/components/SecondPage.vue
@@ -1,6 +1,6 @@
-<template class="bg-red-500">
-  <div class="second">
-    <h1 class="w-full ">{{ msg }}</h1>
+<template>
+  <div class="second" v-if="this.bool===true">
+    <h1 class="w-full" >{{ msg }}</h1>
     <p class="w-full h-full bg-red-600">
       Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde perferendis
       ad ratione excepturi dolore autem minus dicta vel, rem dolorum temporibus,
@@ -16,6 +16,7 @@ export default {
     msg: String,
   },
 };
+
 </script>
 
 <style scoped></style>
