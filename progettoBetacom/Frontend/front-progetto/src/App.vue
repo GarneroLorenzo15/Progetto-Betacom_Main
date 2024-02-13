a268b32f7e6f652a2df8ec89a0e5626ae8570875
GarneroLorenzo15 <garnerolorenzo1@gmail.com>
2024-02-13 16:38:09 +0100

    ..

--- progettoBetacom/Frontend/front-progetto/src/App.vue
+++ progettoBetacom/Frontend/front-progetto/src/App.vue
@@ -9,9 +9,9 @@ body {
   background-image: linear-gradient(to top, #034ea1, #f38120);
   background-repeat: no-repeat;
   background-size: cover;
+  height: 100vh;
+}
+a {
+  color: #f38120;
 }
 </style>
-
-<!-- <script>
-import "../public/main.css";
-</script> -->
--- progettoBetacom/Frontend/front-progetto/src/components/HelloWorld.vue
+++ progettoBetacom/Frontend/front-progetto/src/components/HelloWorld.vue
@@ -1,28 +1,30 @@
 <template>
   <div class="hello">
-    <div class="container">
+    <div class="container my-5">
       <div class="row">
-        <h1 class="d-flex justify-content-center my-5">Esegui Il Login</h1>
+        <h1 class="d-flex justify-content-center">Esegui Il Login</h1>
       </div>
     </div>
     <div class="container">
-      <div class="row w-full" style="height: 200px">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          class="bi bi-person w-full"
-          style="height: 100%, color: #d9d9d9"
-        >
-          <path
-            d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6m2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0m4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4m-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10s-3.516.68-4.168 1.332c-.678.678-.83 1.418-.832 1.664z"
-          />
-        </svg>
+      <div class="row" style="height: 200px">
+        <figure class="w-full d-flex justify-content-center align-items-center">
+          <i class="bi bi-person"></i>
+        </figure>
       </div>
     </div>
-    <div class="container">
+    <div class="container my-5">
       <div class="row d-flex justify-content-center">
         <div>
-          <input class="w-full mb-3" type="text" />
-          <input class="w-full mb-3" type="text" />
+          <input
+            class="w-full mb-3"
+            type="text"
+            placeholder="  inserici email..."
+          />
+          <input
+            class="w-full mb-3"
+            type="text"
+            placeholder="  inserici password(cognome in minuscolo)"
+          />
         </div>
       </div>
     </div>
@@ -33,7 +35,7 @@
         </button>
       </div>
     </div>
-    <div class="container d-flex justify-content-center text-white">
+    <div class="container my-5 d-flex justify-content-center text-white">
       <div class="row">
         <p>Powered by Lorenzo Garnero</p>
       </div>
@@ -52,6 +54,10 @@ export default {
 .w-full {
   width: 100%;
 }
+button,
+input {
+  border-radius: 25px;
+}
 
 h3 {
   margin: 40px 0 0;
@@ -65,6 +71,10 @@ li {
   margin: 0 10px;
 }
 a {
-  color: #42b983;
+  color: #f38120;
+}
+i {
+  color: #d9d9d9;
+  font-size: 10rem;
 }
 </style>
