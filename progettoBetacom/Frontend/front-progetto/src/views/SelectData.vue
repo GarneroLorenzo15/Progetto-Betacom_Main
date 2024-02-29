bb947ec7dc1b04d56392a11bf0f48700f4f0531c
GarneroLorenzo15 <garnerolorenzo1@gmail.com>
2024-02-29 16:46:19 +0100

    Update SelectData.vue

--- progettoBetacom/Frontend/front-progetto/src/views/SelectData.vue
+++ progettoBetacom/Frontend/front-progetto/src/views/SelectData.vue
@@ -1,7 +1,21 @@
 <template>
   <!-- eslint-disable -->
-    <div class="container">
-
+    <div class="data">
+        <div class="container">
+            <div class="d-flex justify-content-center my-5">
+                <h1>Scegli una data</h1>
+            </div>
+            <div class="card my-2">
+                <div class="card-header">
+                    <div class="d-flex justify-content-center">
+                        <h3>giugno</h3>
+                    </div>
+                </div>
+                <div class="card-body">
+                    
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
